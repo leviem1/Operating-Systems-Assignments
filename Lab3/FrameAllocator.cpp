a6efa2c68997b7811c25d3c66a48e150be62642f
Levi Muniz <levi.muniz17@gmail.com>
2019-01-31 03:55:10 -0700

    Added constructor and getter functions
    ADDED - get_available() function
    ADDED - get_available_list_string() function
    CHANGED - Finished constructor code
    CHANGED - Now using namespace std and hex numbers to avoid messiness

--- Lab3/FrameAllocator.cpp
+++ Lab3/FrameAllocator.cpp
@@ -13,26 +13,81 @@
 
 #include "FrameAllocator.h"
 
-FrameAllocator::FrameAllocator(int frameNumber) {
+using namespace std;
     
-    //size the mem vector to the appropriate number of frames and fill with 0's
-    //TODO: needs to be multiplied by page frame size
-    mem.resize(frameNumber * 16384);
-    for(int i = 0; i < mem.size(); i++){
-        if (i % 16384 && i != 0) {
-            mem.at(i) = i + 16380;
-        } else {
-            mem.at(i) = 0;
+FrameAllocator::FrameAllocator(int frameNumber) {
+    //Initialize "system memory" and fill with zeros
+    //0x4000 = 16KB
+    mem.resize(frameNumber * 0x4000, 0);
+    
+    //Create links by storing address of next free frame in first 32-bits
+    for (uint32_t i = 0x4000; i < mem.size(); i += 0x4000) {
+        uint32_t next;
+        
+        //Last value must be 0xFFFFFFFF
+        if (i == mem.size() - 0x4000) {
+            next = 0xFFFFFFFF;
+            memcpy(&mem.at(i), &next, sizeof(next));
+        }
+        
+        //Set value to address of next free frame
+        else {
+            next = i + 0x4000;
+            memcpy(&mem.at(i), &next, sizeof(next));  
         }
     }
     
-    //TODO: Debugger code, should print address of next frame
-    for(int i = 0; i < mem.size(); i += 16384) {
-        std::cout << (int) mem.at(i) << "\n";
+    //Initialize memory allocator variables (stored directly in mem)
+    //Total number of frames in mem
+    uint32_t pageFramesTotal = frameNumber;
+    memcpy(&mem.at(PAGE_FRAMES_TOTAL),
+            &pageFramesTotal, 
+            sizeof(pageFramesTotal));
+    
+    //Number of frames already allocated
+    uint32_t pageFramesAvailable = frameNumber - 1;
+    memcpy(&mem.at(PAGE_FRAMES_AVAILABLE),
+            &pageFramesAvailable,
+            sizeof(pageFramesAvailable));
+    
+    //Address of first available frame
+    uint32_t availableListHead = 0x4000;
+    memcpy(&mem.at(AVAILABLE_LIST_HEAD),
+            &availableListHead,
+            sizeof(availableListHead));   
+}
+
+bool FrameAllocator::Allocate(uint32_t count, vector<uint32_t>& page_frames) {
+    
+}
+
+uint32_t FrameAllocator::get_available() const {
+    return mem.at(PAGE_FRAMES_AVAILABLE);
+}
+
+string FrameAllocator::get_available_list_string() const {
+    uint32_t addr;
+    memcpy(&addr, &mem.at(AVAILABLE_LIST_HEAD), sizeof(addr));
+    
+    return build_string(addr);
+}
+
+string FrameAllocator::build_string(uint32_t addr) const {
+    ostringstream ostring;
+    uint32_t next;
+    memcpy(&next, &mem.at(addr), sizeof (next));
+    
+    //Print this address
+    ostring << " " << hex << addr;
+    
+    //If we're at the end of the list, append new line
+    if (next != 0xFFFFFFFF) {
+        ostring << build_string(next);
     }
+    
+    return ostring.str();
 }
 
 //explicit empty destructor
 FrameAllocator::~FrameAllocator() {
 }
-
--- Lab3/FrameAllocator.h
+++ Lab3/FrameAllocator.h
@@ -16,11 +16,18 @@
 
 #include<vector>
 #include <iostream>
+#include <cstring>
+#include <sstream>
 
 class FrameAllocator {
 public:
     
     FrameAllocator(int frameNumber);
+    
+    bool Allocate(std::uint32_t count, std::vector<std::uint32_t> &page_frames);
+    
+    std::uint32_t get_available() const;
+    std::string get_available_list_string() const;
     
     ~FrameAllocator();
     
@@ -32,6 +39,14 @@ public:
     
 private:
     std::vector<std::uint8_t> mem;
+    //Pointer to PAGE_FRAMES_TOTAL value
+    const static std::uint16_t PAGE_FRAMES_TOTAL = 0;
+    //Pointer to PAGE_FRAMES_AVAILABLE value
+    const static std::uint16_t PAGE_FRAMES_AVAILABLE = 32;
+    //Pointer to AVAILABLE_LIST_HEAD value
+    const static std::uint16_t AVAILABLE_LIST_HEAD = 64;
+    //Recursive function to build available list string
+    std::string build_string(uint32_t addr) const;
 
 };
 
